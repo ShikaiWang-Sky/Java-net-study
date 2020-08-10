a0f2ad00c1d04acc50b5e776530dc8e9bba3375b
hello-world-cn <52561906+hello-world-cn@users.noreply.github.com>
2020-08-10 11:30:45 +0800

    first part of Java net study

---
+++ src/com/wang/chat/TalkReceive.java
@@ -0,0 +1,11 @@
+package com.wang.chat;
+
+/**
+ * @author wang
+ * @creatTime 2020/8/10
+ */
+public class TalkReceive {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/wang/chat/TalkSend.java
@@ -0,0 +1,59 @@
+package com.wang.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+/**
+ * @author wang
+ * @creatTime 2020/8/10
+ */
+public class TalkSend implements Runnable {
+
+    DatagramSocket socket = null;
+    BufferedReader reader = null;
+
+    private int fromPort;
+    private String toIP;
+    private int toPort;
+
+    public TalkSend(String fromIP, String toIP, int toPort) {
+        this.fromPort = fromPort;
+        this.toIP = toIP;
+        this.toPort = toPort;
+
+        try {
+            socket = new DatagramSocket(fromPort);
+            reader = new BufferedReader(new InputStreamReader(System.in));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        //准备数据：控制台读取System.in
+        while (true) {
+            try {
+                String data = reader.readLine();
+                byte[] dataBytes = data.getBytes();
+
+                DatagramPacket packet = new DatagramPacket(dataBytes, 0, dataBytes.length, new InetSocketAddress(this.toIP, this.toPort));
+
+                socket.send(packet);
+
+                if (data.equals("bye"))
+                    break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        socket.close();
+
+    }
+}
